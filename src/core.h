74db6839dfca32fc144c27fdcfdfa902fac43883
elkiwy <elkiwydev@gmail.com>
2020-04-06 21:34:37 +0200

    Changed true costant to simply true

--- src/core.h
+++ src/core.h
@@ -158,7 +158,7 @@ void* searchInEnvironment(List* exp, Environment* env);
 
 // ------------------------------------------------------------------
 //Define what is true and what is false
-#define e_true     cons( intern("quote"), cons( intern("t"), 0))
+#define e_true     intern("true")
 #define e_false    0
 
 
